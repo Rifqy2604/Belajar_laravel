59515b1c6c547c5dfc941db2c3816ff8db79cb81
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-08-11 11:29:33 +0700

    Halo blade

--- resources/views/halo.blade.php
+++ resources/views/halo.blade.php
@@ -8,12 +8,5 @@
 </head>
 <body>
     <h1>Halo ini adalah percobaan</h1>
-    <p>Ini adalah percobaan</p>
-    <p>nama saya rifqy</p>
-    <p>Saya adalah mahasiswa semester 4</p>
-    <p>Jurusan inforamtika</p>
-    <p>Fakultas teknik dan ilmu komputer</p>
-    <p>Universitas Komputer Indonesia</p>
-    <p>saya lahir di bandung</p>
 </body>
 </html>
