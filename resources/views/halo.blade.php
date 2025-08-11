697442cf1f1ac889b6a31d2ed40defb5a6b76be6
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-08-12 05:51:52 +0700

    Membuat blade variable

--- resources/views/halo.blade.php
+++ resources/views/halo.blade.php
@@ -7,6 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Halo ini adalah percobaan</h1>
+    <h1>Nama saya {{ $nama }}</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-route::get('/tes', function () {
-    return 'Halo Ini adalah Routing /tes';
+// Route Get
+route::get('/halo', function () {
+    $nama = "Rifqy Fakhry Zain";
+    return view ("halo", compact("nama"));
 });
 
+// Route Redirect
 route::redirect('/youtube', '/tes');
