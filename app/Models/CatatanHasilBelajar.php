311bf1f1dbbd562473530ed12f9d3e2d84825c8e
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-08-13 06:27:36 +0700

    Buat Isi Model catatan hasil belajar

--- app/Models/CatatanHasilBelajar.php
+++ app/Models/CatatanHasilBelajar.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CatatanHasilBelajar extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = "catatan_hasil_belajar";
+
+    protected $fillable = [
+        'hari_tanggal',
+        'waktu',
+        'materi',
+        'ringkasan',
+        'kesuliran',
+        'link_referensi',
+        'rencana_selanjutnya',
+    ];
 }
