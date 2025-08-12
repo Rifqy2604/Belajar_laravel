643e4ab6fb7419f9122f68a68d7d151ed2cf6a2c
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-08-12 18:37:33 +0700

    Membuat User Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,3 +15,10 @@ route::get('/halo', function () {
 
 // Route Redirect
 route::redirect('/youtube', '/tes');
+
+// Login
+Route::controller(UserController::class)->group(function () {
+    Route::get('/login', 'login');
+    Route::post('/login', 'dologin');
+    Route::post('/logout', 'doLogout');
+});
