fbd05633298e1301d2f1dfcef15105c1a4e88514
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-08-13 06:23:58 +0700

    Buat isi migrasi

--- database/migrations/2025_08_12_231833_create_catatan_hasil_belajar_table.php
+++ database/migrations/2025_08_12_231833_create_catatan_hasil_belajar_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use function Laravel\Prompts\table;
+
 return new class extends Migration
 {
     /**
@@ -13,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('catatan_hasil_belajar', function (Blueprint $table) {
             $table->id();
+            $table->date('hari_tanggal');
+            $table->time('waktu')->nullable();
+            $table->string('materi',255);
+            $table->text('ringkasan')->nullable();
+            $table->string('kesulitan', 50)->nullable();
+            $table->text('link_referensi')->nullable();
+            $table->text('rencana_selanjutnya')->nullable();
             $table->timestamps();
         });
     }
