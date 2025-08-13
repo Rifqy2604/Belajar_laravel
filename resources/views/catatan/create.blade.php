d63374f10b05e4220830b03c3cee4b3d380d1ce0
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-08-13 18:53:10 +0700

    Mengubah Blade php

--- resources/views/catatan/create.blade.php
+++ resources/views/catatan/create.blade.php
@@ -8,7 +8,7 @@
         @csrf
 
         <div>
-            <label class="block font-medium"></label>
+            <label class="block font-medium">Tanggal</label>
             <input type="date" name="hari_tanggal" class="w-full border rounded px-3 py-2" required>
         </div>
 
