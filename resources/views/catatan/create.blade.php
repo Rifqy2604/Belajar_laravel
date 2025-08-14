8241589de12f2d98e4abd593566a57b50fee7c89
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-08-14 20:48:24 +0700

    Tes

--- resources/views/catatan/create.blade.php
+++ resources/views/catatan/create.blade.php
@@ -13,7 +13,7 @@
         </div>
 
         <div>
-            <label class="block font-medium">Waktu</label>
+            <label class="block font-medium"></label>
             <input type="time" name="waktu" class="w-full border rounded px-3 py-2">
         </div>
 
