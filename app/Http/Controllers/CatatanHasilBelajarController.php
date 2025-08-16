76e9a460f7701237e758c12420421bbb4b43fda9
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-08-16 12:57:35 +0700

    ubah controller

--- app/Http/Controllers/CatatanHasilBelajarController.php
+++ app/Http/Controllers/CatatanHasilBelajarController.php
@@ -20,61 +20,15 @@ class CatatanHasilBelajarController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+
+    public function tambah()
     {
-        $validate = $request->validate([
-            "hari_tanggal" => "required|date",
-            "waktu" => "nullable|date_format:H:i",
-            "materi" => "required|string|max:255",
-            "ringkasan" => "nullable|string",
-            "kesulitan" => "nullable|string|max:50",
-            "link_referensi" => "nullable|string",
-            "rencana_selanjutnya" => "nullable|string",
-        ]);
-
-        $catatan = CatatanHasilBelajar::create($request->all());
-
-        return response()->json($catatan, 201);
+        return view('catatan.tambah');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
-    {
-        return response()->json(CatatanHasilBelajar::findOrFail($id));
-    }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        $catatan = CatatanHasilBelajar::findOrFail($id);
-
-        $validated = $request->validate([
-            "hari_tanggal" => "required|date",
-            "waktu" => "nullable|date_format:H:i",
-            "materi" => "required|string|max:255",
-            "ringkasan" => "nullable|string",
-            "kesulitan" => "nullable|string|max:50",
-            "link_referensi" => "nullable|string",
-            "rencana_selanjutnya" => "nullable|string",
-        ]);
-
-        $catatan->update($validated);
-
-        return response()->json($catatan);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        $catatan = CatatanHasilBelajar::finOrFall($id);
-        $catatan->delete();
-
-        return response()->json(null,0);
-    }
+    
 }
