2ff1983bb69ccf39d65b4201cfe64d3735560c2b
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-08-16 14:29:48 +0700

    Membuat Blade Tampil

--- resources/views/catatan/tampil.blade.php
+++ resources/views/catatan/tampil.blade.php
@@ -95,7 +95,7 @@
                                         @csrf
                                         <button type="submit"
                                             class="text-white bg-red-400 hover:bg-red-500 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full 
-                                            text-sm px-2 py-1.5 text-center dark:focus:ring-red-300">Delete</button>
+                                            text-sm px-2 py-1.5 text-center dark:focus:ring-red-300">Deletee</button>
                                     </form>
                                 </div>
                             </td>
